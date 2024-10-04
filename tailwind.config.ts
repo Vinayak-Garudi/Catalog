4bc0155855e64d016d79ae7eed3ad445b40e9322
Vinayak Garudi <vinayak.vslmarine@gmail.com>
2024-10-04 23:45:49 +0530

    initial commit with setting of colours

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,19 +1,27 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
-  },
-  plugins: [],
+	content: [
+		"./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+		"./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+		"./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+	],
+	theme: {
+		extend: {
+			colors: {
+				background: "var(--background)",
+				foreground: "var(--foreground)",
+				primary: "var(--primary)",
+				primaryHover: "var(--primaryHover)",
+				secondary: "var(--secondary)",
+				secondaryHover: "var(--secondaryHover)",
+				success: "var(--success)",
+				successHover: "var(--successHover)",
+				danger: "var(--danger)",
+				dangerHover: "var(--dangerHover)",
+			},
+		},
+	},
+	plugins: [],
 };
 export default config;
